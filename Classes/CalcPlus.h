40196fa1fdcc6689dae0e28dd0cd43cb49cc3063
chenshengzhi <chenshengzhi@hudunsoft.com>
2019-08-15 18:16:24 +0800

    no message

---
+++ Classes/CalcPlus.h
@@ -0,0 +1,20 @@
+//
+//  CalcPlus.h
+//  CalcPlus
+//
+//  Created by mac68 on 2019/8/15.
+//  Copyright © 2019 mac68. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CalcPlus : NSObject
+
+- (NSInteger)plus:(NSInteger)a withB:(NSInteger) b;
+- (NSInteger)minus:(NSInteger)a withB:(NSInteger) b;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/CalcPlus.m
@@ -0,0 +1,21 @@
+//
+//  CalcPlus.m
+//  CalcPlus
+//
+//  Created by mac68 on 2019/8/15.
+//  Copyright © 2019 mac68. All rights reserved.
+//
+
+#import "CalcPlus.h"
+
+@implementation CalcPlus
+
+- (NSInteger)plus:(NSInteger)a withB:(NSInteger) b {
+    return a + b;
+}
+
+- (NSInteger)minus:(NSInteger)a withB:(NSInteger) b {
+    return a - b;
+}
+
+@end
